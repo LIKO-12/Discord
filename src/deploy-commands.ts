202e033274687283ee5047de20c276e797fdf43e
Danny Mill <benacker519@gmail.com>
2021-09-06 19:21:46 +0200

    Adding /method
     * added devDeplouCommmands which runs ts-node instead of node

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -5,7 +5,10 @@ import { Routes } from'discord-api-types/v9';
 import config from './config';
 
 const commands: Array<any> /*idk what the interface for json slash commands are*/ = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!')
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+    new SlashCommandBuilder().setName('method').setDescription('Replies with method') // TODO: better description
+    .addStringOption(option => option.setName('method_name').setDescription('Method Name').setRequired(true))
+    .addIntegerOption(option => option.setName('usage_id').setDescription('Usage Id').setRequired(false))
 ].map(command => command.toJSON());
 
 const rest: REST = new REST({ version: '9' }).setToken(config.botToken);
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,100 @@
-import { Client, Intents } from 'discord.js';
+import Discord from 'discord.js';
+import { createMethodEmbed, methodsIndex } from './lib/doc-utils';
+
 import config from './config';
 
 
-const client: Client = new Client({
+const client: Discord.Client = new Discord.Client({
   intents: [],
   presence: {
     status: 'online',
     activities: [{
-      name: ".method",
+      name: "/method",
       type: 'WATCHING'
     }]
   }
 });
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
+// const methodUsageEmbed = new Discord.MessageEmbed()
+//   .setTitle('Usage')
+//   .setDescription('```css\n.method <method_name> [usage_id]\n```');
+
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+  const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong 🏓');
-	};
+  if (commandName === 'ping') {
+    await interaction.reply('Pong 🏓');
+  }
+  else if (commandName === 'method') { // it does not seem to work the same as .method, not sure why, this is kinda confusing and uncommented.
+    const methodName = interaction.options.getString('method_name', true);
+    const usageId = interaction.options.getInteger('usage_id', false) ?? -1;
+
+    // if (!methodName) return message.channel.send('', methodUsageEmbed.setFooter(`@${message.author.username}#${message.author.discriminator}`));
+
+    const plainName = !(/[\.:]/.test(methodName));
+
+    let selectedMatch = undefined;
+    const shorterMatches = [], longerMatches = [];
+    for (const index in methodsIndex) {
+      //Found an exact match
+      if (index === methodName) {
+        selectedMatch = methodsIndex[index];
+        break;
+      }
+
+      if (!plainName || !/[\.:]/.test(index)) {
+        //Longer match
+        if (index.includes(methodName)) longerMatches.push(methodsIndex[index]);
+
+        //Shorter match
+        if (methodName.includes(index)) shorterMatches.push(methodsIndex[index]);
+      }
+    }
+
+    //Found an exact match
+    if (selectedMatch) return interaction.reply({ embeds: [createMethodEmbed(selectedMatch.peripheral, selectedMatch.object, selectedMatch.name, selectedMatch.method, usageId)] });
+
+    //Found a single longer match. this code is ugly ngl.
+    if (longerMatches.length === 1) {
+      selectedMatch = longerMatches[0];
+      const embed = createMethodEmbed(selectedMatch.peripheral, selectedMatch.object, selectedMatch.name, selectedMatch.method, usageId);
+      const extendFooter = `Didn't find an exact match for '${methodName}' but instead found a longer match.`
+      embed.setFooter(embed.footer?.text ? `${embed.footer.text}\n${extendFooter}` : extendFooter);
+      interaction.reply({ embeds: [embed] });
+      return
+    }
+
+    //No longer matches, and a single shorter match
+    if (longerMatches.length === 0 && shorterMatches.length === 1) {
+      selectedMatch = shorterMatches[0];
+      const embed = createMethodEmbed(selectedMatch.peripheral, selectedMatch.object, selectedMatch.name, selectedMatch.method, usageId);
+      const extendFooter = `Didn't find an exact match for '${methodName}' but instead found a shorter match.`
+      embed.footer = embed.footer ?? {};
+      embed.footer.text = embed.footer.text ? `${embed.footer.text}\n${extendFooter}` : extendFooter;
+      interaction.reply({ embeds: [embed] });
+      return
+    }
+
+    const embed = new Discord.MessageEmbed();
+
+    if (shorterMatches.length === 0 && longerMatches.length === 0) {
+      embed.title = 'No results found ⚠';
+    } else {
+      embed.title = 'No exact match';
+      embed.description = 'But found those shorter/longer matches:';
+
+      if (longerMatches.length > 0) embed.addField('Longer matches', longerMatches.map(match => `- \`${match.formatted}\``).join('\n'));
+      if (shorterMatches.length > 0) embed.addField('Shorter matches', shorterMatches.map(match => `- \`${match.formatted}\``).join('\n'));
+    }
+
+    interaction.reply({ embeds: [embed] });
+  };
 });
 
 client.login(config.botToken);
